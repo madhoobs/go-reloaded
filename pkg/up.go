29c9bd130c39b1b235b2b5c9cfb874357eedf40c
Hasan Almadhoob <hasan@madhoob.com>
2024-08-14 00:20:09 +0300

    Cleaning code

--- pkg/up.go
+++ pkg/up.go
@@ -33,19 +33,3 @@ func Uppercase(text string) string {
 
 	return strings.Join(words, " ")
 }
-
-// func Uppercase(text string) string {
-// 	if len(text) == 0 {
-// 		return text
-// 	}
-
-// 	regex := `(\w+)\s*\(up\)`
-// 	re := regexp.MustCompile(regex)
-
-// 	result := re.ReplaceAllStringFunc(text, func(match string) string {
-// 		word := re.FindStringSubmatch(match)[1]
-// 		return strings.ToUpper(word)
-// 	})
-
-// 	return result
-// }
